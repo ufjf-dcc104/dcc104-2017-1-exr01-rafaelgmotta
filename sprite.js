e93c53bb0935674d173a6b3ef1423fabde6dc415
Rafael Motta <rafaelgmotta@gmail.com>
2017-04-05 19:04:00 -0300

    animacao

--- sprite.js
+++ sprite.js
@@ -6,6 +6,7 @@ function Sprite(){
   this.ax=0;
   this.ay=0;
   this.color = "blue";
+  this.g = 0;
 }
 
 Sprite.prototype.desenhar = function (ctx) {
@@ -16,9 +17,25 @@ Sprite.prototype.desenhar = function (ctx) {
 };
 
 Sprite.prototype.mover = function (dt){
+  dt=this.g;
   this.vx = this.vx + this.ax*dt;
   this.vy = this.vy + (this.ay+60)*dt;
   this.x = this.x + this.vx*dt;
   this.y = this.y + this.vy*dt;
 
+}
+
+Sprite.prototype.colidiuCom = function (alvo)
+{
+  if(this.x+15 < alvo.x) return false;
+  if(this.x > alvo.x+15) return false;
+  if(this.y+15 < alvo.y) return false;
+  if(this.y > alvo.y+15) return false;
+  return true;
+}
+
+Sprite.prototype.perseguir = function (alvo, dt)
+{
+    this.ax += 10*dt*(alvo.x - this.x) - 0.05 * this.vx;
+    this.ay += 10*dt*(alvo.y - this.y) - 0.05 * this.vy;
 }
